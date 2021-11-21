460acd153f08b5a579310d4187866f1a1a8f0220
MatejPapac <76448484+MatejPapac@users.noreply.github.com>
2021-11-22 00:13:36 +0100

    Update Rad.sql

--- Rad.sql
+++ Rad.sql
@@ -3,13 +3,14 @@ create database zavrsni character set utf8;
 use zavrsni;
 
 create table bistroLoora(
-    sifra int not null primary key auto_increment,
-    jelovnik int not null,
-    vinska karta int not null,
-    rezervacija varchar(50) ,
-    dostava boolean,
-    nacinPlacanja varchar(50)
-);
+        sifra  int not null primary key auto_increment,
+        jelovnik  int not null,
+        vinskaKarta  int not null,
+        rezervacija  int not null ,
+        nacinPlacanja  varchar(50),
+        dostava  boolean
+       
+    );
 
 create table jelovnik(
 
@@ -28,11 +29,15 @@ brojRezerviranihMjesta varchar(50) not null,
 razlog varchar(50)
 );
 
-create table vinska karta(
+create table vinskaKarta(
     sifra int not null primary key auto_increment,
     bijelo varchar(50),
     crno varchar(50),
     misno varchar(50),
     korlat varchar(50)
 );
+alter table bistroLoora add foreign key (jelovnik) references jelovnik(sifra);
+alter table bistroLoora add foreign key (rezervacija) references rezervacija(sifra);
+alter table bistroLoora add foreign key (vinskaKarta) references vinskaKarta(sifra);
+
 
