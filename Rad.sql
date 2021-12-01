28b8d3799d73a76296149714dcefbff7e91a62a0
MatejPapac <76448484+MatejPapac@users.noreply.github.com>
2021-12-01 14:32:17 +0100

    Ubacio inserte izbacio jelovnik

--- Rad.sql
+++ Rad.sql
@@ -5,7 +5,7 @@ use zavrsni;
 create table jelo(
         sifra  int not null primary key auto_increment,
         naziv  varchar(50) not null,
-        cijena varchar(50) not null,
+        cijena decimal(16,2) not null,
         vrsta  int not null 
        
        
@@ -14,7 +14,7 @@ create table jelo(
 create table pice(
         sifra int not null primary key auto_increment,
         naziv varchar(50) not null,
-        cijena varchar(50) not null,
+        cijena decimal(16,2) not null,
         vrsta int not null
 
 
@@ -33,7 +33,6 @@ create table pice(
 create table mjesto(
         sifra int not null primary key  auto_increment,
         naziv varchar(50) not null,
-        rezervacija boolean,
         brojStolica varchar(50)
   
 
@@ -58,7 +57,32 @@ alter table rezervacija_mjesto add foreign key (mjesto) references rezervacija(s
 alter table rezervacija_mjesto add foreign key (rezervacija) references mjesto(sifra);
 
 
+insert into vrsta(sifra,naziv)values
+(null,'Sok'),
+(null,'Zesta'),
+(null,'Gazirana pica');
 
+insert into pice(sifra,naziv,cijena,vrsta)
+values(null,'Cedevita',9.00,1),
+       (null,'Pelinkovac 0,03',8.00,2),
+       (null,'Coca-Cola',14.00,3);
+
+
+insert into rezervacija(sifra,datumiVrijeme,osoba,brojRezerviranihMjesta,mjesto,kontakt,napomena)
+values(null,'12-2-2019','Miljenko',12,1,'Goformiljenko@gmail.com','abcd'),
+(null,'13-2-2019','Andrija',8,2,'GoforAndrija@gmail.com','abcd'),
+(null,'14-2-2019','Vjekoslav',24,3,'GoforVjekoslav@gmail.com','abcd');
+
+insert into mjesto (sifra,naziv,brojStolica)
+values(null,'Stol 101','4'),
+      (null, 'Stol 102','5'),
+      (null,'Stol 103','6');
+
+      insert into rezervacija_mjesto(sifra, rezervacija,mjesto)
+      values(null,1,1),
+      (null,2,2),
+      (null ,1,3);
+         
 
 
 
